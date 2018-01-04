f8f4878a3bf2ba27a88b5042d38df897239b95f2
Eliot Swank <eliotswank@gmail.com>
2018-01-04 12:49:23 -0700

    Headers method finished

--- lib/markdown_converter.rb
+++ lib/markdown_converter.rb
@@ -7,7 +7,7 @@ attr_reader :markdown_text
     @markdown_text = markdown_text
   end
 
-  def paragraph
+  def paragraphs
       @markdown_text = "<p>#{markdown_text}</p>"
       if @markdown_text.include?("\n\n")
         @markdown_text.gsub("\n\n", "</p>\n<p>")
@@ -16,6 +16,28 @@ attr_reader :markdown_text
       else
         @markdown_text
       end
+  end
 
+  def headers
+    if @markdown_text.start_with?("# ")
+      @markdown_text.slice!("# ")
+      @markdown_text = "<h1>#{markdown_text}</h1>"
+    elsif @markdown_text.start_with?("## ")
+      @markdown_text.slice!("## ")
+      @markdown_text = "<h2>#{markdown_text}</h2>"
+    elsif markdown_text.start_with?("### ")
+      @markdown_text.slice!("### ")
+      @markdown_text = "<h3>#{markdown_text}</h3>"
+    elsif markdown_text.start_with?("#### ")
+      @markdown_text.slice!("#### ")
+      @markdown_text = "<h4>#{markdown_text}</h4>"
+    elsif markdown_text.start_with?("##### ")
+      @markdown_text.slice!("###### ")
+      @markdown_text = "<h5>#{markdown_text}</h5>"
+    elsif markdown_text.start_with?("###### ")
+      @markdown_text.slice!("###### ")
+      @markdown_text = "<h6>#{markdown_text}</h6>"
+
+    end
   end
 end
--- test/markdown_converter_test.rb
+++ test/markdown_converter_test.rb
@@ -20,18 +20,24 @@ class MarkDownConverterTest < Minitest::Test
   def test_it_adds_html_paragraph_symbols
     converter = MarkDownConverter.new("This is the first line of a paragraph.")
 
-    assert_equal "<p>This is the first line of a paragraph.</p>", converter.paragraph
+    assert_equal "<p>This is the first line of a paragraph.</p>", converter.paragraphs
   end
 
   def test_it_handles_single_line_breaks
     converter = MarkDownConverter.new("This is the first line of the paragraph.\nThis is the second line of the same paragraph.")
 
-    assert_equal "<p>This is the first line of the paragraph. This is the second line of the same paragraph.</p>", converter.paragraph
+    assert_equal "<p>This is the first line of the paragraph. This is the second line of the same paragraph.</p>", converter.paragraphs
   end
 
   def test_it_can_break_into_multiple_paragraphs
     converter = MarkDownConverter.new("This is the first line of the first paragraph.\n\nThis is the first line of the second paragraph.")
 
-    assert_equal "<p>This is the first line of the first paragraph.</p>\n<p>This is the first line of the second paragraph.</p>", converter.paragraph
+    assert_equal "<p>This is the first line of the first paragraph.</p>\n<p>This is the first line of the second paragraph.</p>", converter.paragraphs
+  end
+
+  def test_it_adds_headers
+    converter = MarkDownConverter.new("## Here's an H2")
+
+    assert_equal "<h2>Here's an H2</h2>", converter.headers
   end
 end
