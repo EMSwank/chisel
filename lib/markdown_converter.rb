41b3054a7a358283962008919c1798678d73cce4
Eliot Swank <eliotswank@gmail.com>
2018-01-04 12:02:27 -0700

    Paragraph method working

--- lib/markdown_converter.rb
+++ lib/markdown_converter.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class MarkDownConverter
 
 attr_reader :markdown_text
@@ -7,7 +8,14 @@ attr_reader :markdown_text
   end
 
   def paragraph
-    @markdown_text = "<p>#{markdown_text}</p>"
-      markdown_text.gsub("\n", " ")
+      @markdown_text = "<p>#{markdown_text}</p>"
+      if @markdown_text.include?("\n\n")
+        @markdown_text.gsub("\n\n", "</p>\n<p>")
+      elsif @markdown_text.include?("\n")
+        @markdown_text.gsub("\n", " ")
+      else
+        @markdown_text
+      end
+
   end
 end
--- test/markdown_converter_test.rb
+++ test/markdown_converter_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 require './lib/markdown_converter.rb'
 
 class MarkDownConverterTest < Minitest::Test
@@ -23,9 +24,14 @@ class MarkDownConverterTest < Minitest::Test
   end
 
   def test_it_handles_single_line_breaks
-    converter = MarkDownConverter.new("This is the first line of the paragraph.
-This is the second line of the same paragraph.")
+    converter = MarkDownConverter.new("This is the first line of the paragraph.\nThis is the second line of the same paragraph.")
 
     assert_equal "<p>This is the first line of the paragraph. This is the second line of the same paragraph.</p>", converter.paragraph
+  end
+
+  def test_it_can_break_into_multiple_paragraphs
+    converter = MarkDownConverter.new("This is the first line of the first paragraph.\n\nThis is the first line of the second paragraph.")
+
+    assert_equal "<p>This is the first line of the first paragraph.</p>\n<p>This is the first line of the second paragraph.</p>", converter.paragraph
   end
 end
